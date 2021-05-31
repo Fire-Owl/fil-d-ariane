a5aa2ac654680b3db3f37f59c3737501ba5f692e
etiennezastko <etiennezastko@hotmail.fr>
2021-05-31 13:53:42 +0200

    first commit

---
+++ pages/à-propos.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+
+<body>
+    <!-- ouvre container -->
+    <div class="container">
+        <h1 class="element__titre1">À propos</h1>
+        <!-- ouvre container projet-et-image -->
+        <div class="container__projet-et-image">
+            <!-- ouvre container__projet -->
+            <div class="container__projet">
+                <h1 class="element__titre2">Le projet</h1>
+                <p>
+                    Ce site internet est <br>
+                    l’aboutissement d’un projet de
+                    groupe dédié à l’illectronisme.<br><br>
+                    Nous l’avons conçu lors de notre
+                    formation de web designer au
+                    sein de l’Accesss Code School de
+                    Dijon.
+                </p>
+            </div> <!-- ferme container projet -->
+
+            <img src="../assets/images/à-propos.svg" alt="à-propos.svg" id="img_a-propos">
+        </div> <!-- ferme container__projet-et-image -->
+    </div> <!-- ferme container -->
+
+    <div>
+        <h1 class="element__titre3">L'équipe
+        </h1>
+    </div>
+
+    <div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
