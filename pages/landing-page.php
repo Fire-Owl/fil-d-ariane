16bdb848c6bade8d2adc8078fb092e151d274e6a
acs <k.el-yacoubi@codeur.online>
2021-05-31 16:54:01 +0200

    16h53

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -6,7 +6,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Document</title>
-<link rel="stylesheet" href="../assets/styles/main.css">
+<link rel="stylesheet" href="../assets/styles/landing-page.css">
+<?php include '../includes/header.php'; ?>
 </head>
 
 <body>
@@ -26,4 +27,23 @@
     </div> <!-- ferme container__images-informations-->
 </div> <!-- ferme container -->
 
+<div class="container-2">
+    <div class="container_images-preconisations">
+        <img src="../assets/images/Online world-rafiki.svg" alt="online world rafiki.svg" id="online">
+    </div> <!-- ferme container__images-preconisations-->
+
+    <div class="container_preconisations">
+        <p class="texte-preconisations">Nous avons constaté que malgré une utilisation quotidienne de notre part des outils numérique et d’Internet, certains éprouvaient parfois du mal avec ces outils et la question qui nous est naturellement venu à l’esprit</p>
+
+       <p class="Internet">Qu'en est-il des personnes n’ayant pas grandi avec Internet et l’essor du numérique ? ».</p> 
+       <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
+
+   
+    </div> <!-- ferme container informations -->
+
+</div> <!-- ferme container-2-->
+
+</body>
+
+<?php include '../includes/footer.php'; ?>
 
