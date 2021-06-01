efe54d1a0cda665432a97cd1f3172fb172e0db3e
acs <k.el-yacoubi@codeur.online>
2021-06-01 15:52:04 +0200

    landing page update

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -18,8 +18,6 @@
 
        <p class="stagiaires"> Nous sommes une équipe de 4, stagiaires<br> à l’Access Code School de Dijon, notre<br> projet à pour but de sensibiliser les<br> développeurs, les intégrateurs web et le<br> public à <span class="access">l’accessibilité numérique.</span></p> 
        <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
-
-   
     </div> <!-- ferme container informations -->
     
     <div class="container_images-informations">
@@ -27,21 +25,37 @@
     </div> <!-- ferme container__images-informations-->
 </div> <!-- ferme container -->
 
-<div class="container-2">
-    <div class="container_images-preconisations">
-        <img src="../assets/images/Online world-rafiki.svg" alt="online world rafiki.svg" id="online">
-    </div> <!-- ferme container__images-preconisations-->
 
-    <div class="container_preconisations">
-        <p class="texte-preconisations">Nous avons constaté que malgré une utilisation quotidienne de notre part des outils numérique et d’Internet, certains éprouvaient parfois du mal avec ces outils et la question qui nous est naturellement venu à l’esprit</p>
+<div class="image-preconisations">
+        <img src="../assets/images/Online world-rafiki.svg" alt="search engines rafiki.svg" id="engines">
+</div><!--ferme div image-->
 
-       <p class="Internet">Qu'en est-il des personnes n’ayant pas grandi avec Internet et l’essor du numérique ? ».</p> 
-       <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
+<div class="texte-preconisations">
+        <p class="Outils">Nous avons constaté que malgré une utilisation quotidienne de notre part des outils numérique et d’Internet, certains éprouvaient parfois du mal avec ces outils et une question nous est naturellement venu à l’esprit:</p><br>
+        <p class="Internet">Qu'en est-il des personnes n’ayant pas grandi avec Internet et l’essor du numérique ? </p>
+        <a href="../pages/"><button class="bouton" type="button"> Préconisations</button></a>
 
-   
+</div>
+
+
+
+
+
+<div class="container">
+    <div class="container__lexique">
+        <p class="texte-lexique"> Nous nous sommes penchés sur cette problématique et<br> choisi de nous orienter vers le sujet de <span class="illectronisme">l’illectronisme.</span><br>
+         En cette période de trouble sanitaire et de confinement,<br> nous avons tous eu dans notre entourage, une personne<br> âgée qui s’est retrouvé isolé car elle ne pouvait pas utiliser<br> de nouveaux moyens de communications.<br> 
+         De ce constat amer, nous avons décidé entre autres de parler<br> de sensibiliser au sujet de l’illectronisme, en se concentrant<br> sur l’accessibilité des séniors.<br>
+        Vous trouverez sur ce site divers informations de sensibilisations<br> et de préconisations sur le sujet de l’illectronisme. 
+        </p>
+        <a href="../pages/"><button class="bouton" type="button"> Lexique</button></a>
+
     </div> <!-- ferme container informations -->
-
-</div> <!-- ferme container-2-->
+    
+    <div class="container_images-informations">
+        <img src="../assets/images/Mention-rafiki.svg" alt="search engines rafiki.svg" id="engines">
+    </div> <!-- ferme container__images-informations-->
+</div> <!-- ferme container -->
 
 </body>
 
