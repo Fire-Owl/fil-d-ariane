9ed7608f75f0471a18bf13c990a3ffd3cd17ce34
JbDruet <jb.druet@codeur.online>
2021-06-01 15:43:25 +0200

    FilSurHeader

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,3 +6,13 @@ function mobileMenu() {
       nav.className = "topnav";
   }
 }
+
+/* script pour le fil */
+
+window.onscroll = function() { scrollIndicator() } ;
+function scrollIndicator() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("img").style.strokeDashoffset = (3100)-scrolled + "vw";
+      };
