3ae4a71add321701d0affb794bb4c4a9da3d97b1
JbDruet <jb.druet@codeur.online>
2021-06-01 15:45:47 +0200

    changeVWtoREM

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,5 +14,5 @@ function scrollIndicator() {
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
         var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         var scrolled = (winScroll / height) * 100;
-        document.getElementById("img").style.strokeDashoffset = (3100)-scrolled + "vw";
+        document.getElementById("img").style.strokeDashoffset = (3100)-scrolled + "rem";
       };
