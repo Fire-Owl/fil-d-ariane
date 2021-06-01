6f5240e47fbaaf87d531e852c3f647dda026d954
acs <k.el-yacoubi@codeur.online>
2021-06-01 16:27:50 +0200

    update 16h27

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -16,8 +16,8 @@
     <div class="container__informations">
         <p class="bonjour">Bonjour chers voyageurs du web, bienvenue au bout du <span class="fil">« Fil <br> d’Ariane ».<br></span></p>
 
-       <p class="stagiaires"> Nous sommes une équipe de 4, stagiaires<br> à l’Access Code School de Dijon, notre<br> projet à pour but de sensibiliser les<br> développeurs, les intégrateurs web et le<br> public à <span class="access">l’accessibilité numérique.</span></p> 
-       <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
+        <p class="stagiaires"> Nous sommes une équipe de 4, stagiaires<br> à l’Access Code School de Dijon, notre<br> projet à pour but de sensibiliser les<br> développeurs, les intégrateurs web et le<br> public à <span class="access">l’accessibilité numérique.</span></p> 
+        <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
     </div> <!-- ferme container informations -->
     
     <div class="container_images-informations">
@@ -27,7 +27,7 @@
 
 
 <div class="image-preconisations">
-        <img src="../assets/images/Online world-rafiki.svg" alt="search engines rafiki.svg" id="engines">
+        <img src="../assets/images/Online world-rafiki.svg" alt="Online world-rafiki.svg" id="world">
 </div><!--ferme div image-->
 
 <div class="texte-preconisations">
@@ -43,7 +43,8 @@
 
 <div class="container">
     <div class="container__lexique">
-        <p class="texte-lexique"> Nous nous sommes penchés sur cette problématique et<br> choisi de nous orienter vers le sujet de <span class="illectronisme">l’illectronisme.</span><br>
+        <p class="texte-lexique"> Nous nous sommes penchés sur cette problématique et<br> choisi de nous orienter vers le sujet de <span class="illectronisme">l’illectronisme.<br></span>
+        
          En cette période de trouble sanitaire et de confinement,<br> nous avons tous eu dans notre entourage, une personne<br> âgée qui s’est retrouvé isolé car elle ne pouvait pas utiliser<br> de nouveaux moyens de communications.<br> 
          De ce constat amer, nous avons décidé entre autres de parler<br> de sensibiliser au sujet de l’illectronisme, en se concentrant<br> sur l’accessibilité des séniors.<br>
         Vous trouverez sur ce site divers informations de sensibilisations<br> et de préconisations sur le sujet de l’illectronisme. 
@@ -52,8 +53,8 @@
 
     </div> <!-- ferme container informations -->
     
-    <div class="container_images-informations">
-        <img src="../assets/images/Mention-rafiki.svg" alt="search engines rafiki.svg" id="engines">
+    <div class="container_images-lexique">
+        <img src="../assets/images/Mention-rafiki.svg" alt="Mention-rafiki.svg" id="mention">
     </div> <!-- ferme container__images-informations-->
 </div> <!-- ferme container -->
 
