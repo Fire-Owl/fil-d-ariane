f1d5b1c35636aa1f2ecaa3cddcc975595147f937
nathbout2 <nathbout2@gmail.com>
2021-06-01 15:32:27 +0200

    lexique

---
+++ pages/lexique.php
@@ -0,0 +1,114 @@
+<?php include '../includes/header.php'; ?>
+<div class='contenaire_lexique'>
+    <h1 class=titre1>Lexique</h1>
+        <p>Vous êtes souvent perdu avec tout ce jargon numérique? Vous avez l’impression de déchiffrer une langue étrangère? Pas de panique! Voici un court lexique couvrant quelques termes couramment rencontrés, ansi que des ressources externes utiles!</p>
+    <h2 class='titre2'>Sommaire</h2>
+        <li>Le matériel</li>
+        <li>Le vocabulaire</li>
+        <li>Pour aller plus loin</li>
+
+    <div class="contenaire_matériel">
+        <h4>Périphérique :</h4>
+        <p>Matériel que l’on connecte à un ordinateur, y ajoutant des fonctionnalités. On distingue les périphériques d’entrée, qui vont envoyer des informations vers l’ordinateur (comme une souris ou un clavier) et les périphériques de sortie, qui reçoivent des informations de l’ordinateur (comme un écran ou une imprimante).</p>
+
+        <h4>Pavé numérique :</h4>
+        <p> Ensemble de touches le plus souvent situé à droite du clavier, parmi lesquelles on trouve les nombres de 0 à 9 et des touches de calcul basique.</p>
+
+        <h4>Hardware :</h4>
+        <p>Anglais pour matériel. </p>
+
+        <h4>USB (Universal Serial Bus) :</h4>
+        <p>Correspond le plus souvent à un type de fiche très répandu utilisé pour connecter ses périphériques à un ordinateur ou tout autre terminal. Il en existe plusieurs types et normes.</p>
+
+        <h4>Cable Ethernet ou RJ45 : </h4>
+        <p> Le câble réseau majoritairement utilisé pour relier son ordinateur à une box internet.</p>
+        
+        <h4>Plug and Play :</h4>
+
+        <p>Un périphérique dit Plug and Play est un périphérique qui fonctionne dès qu’on le branche, sans installation de logiciel préalable.</p>
+
+        <h4>Wifi (Wireless Fidelity) :</h4>
+        <p> Un type de réseau sans fil local, qui permet entre-autre de se connecter à internet sans fil.</p>
+
+        <h4>Bluetooth:</h4>
+        <p>Norme de communication sans fil permettant à divers matériels d’échanger des informations via les ondes radio. </p>
+
+        <h4>ADSL (Asymmetric Digital Subscriber Line) : </h4>
+        <p>Technique de communication permettant d’obtenir internet via le réseau téléphonique.</p>
+
+        <h4> Box internet : </h4>
+        <p>Boitier qui permet de faire le lien entre l’utilisateur final et le fournisseur d’accès internet. C’est à celui-ci que l’on connecte le matériel pour utiliser internet.</p>
+
+    </div>
+    <h2 class='titre2'>Le vocabulaire</h2>
+    <h4>Système d’exploitation :</h4> 
+    <p>Ensemble de programmes qui permet à l’ordinateur de fonctionner correctement et d’utiliser les ressources à sa disposition. Il existe de nombreux systèmes d’exploitation, tels que Windows, Mac OS, ou les distributions Linux.</p> 
+
+    <h4>Bureau :</h4>
+    <p>L’écran où l’utilisateur arrive lorsque l’ordinateur a chargé son système d’exploitation. Il contient le plus souvent des raccourcis vers les programmes utilisés par l’utilisateur.</p>
+
+    <h4>Raccourci :</h4>
+    <p>Petit fichier qui redirige vers un fichier plus gros sur l’ordinateur. Cela permet de lancer des programmes ou d’ouvrir des dossiers sans avoir à chercher leur emplacement à chaque fois.</p>
+
+    <h4>Barre des tâches :</h4>
+    <p>Barre se situant en bas du bureau, affichant les différents programmes ouverts, la date, ainsi que d’autres fonctionnalités, comme l’arrêt de l’ordinateur ou une barre de recherche.</p>
+
+    <h4>Octets (bytes) et bits :</h4> 
+    <p>Unités de mesure informatique, qui correspond à la taille que prend les éléments sur le matériel. 1 octet ou byte = 8 bits; 1 Mo (mégaoctet) = 1000000 octet; 1 Go (gigaoctet) = 1000 Mo; 1000 Go = 1 To 5 (teraoctet), etc.…</p>
+
+    <h4>Navigateur :</h4>
+    <p>Logiciel qui permet de naviguer sur internet.</p>
+
+    <h4>Scroll :</h4>
+    <p>Le fait de faire défiler du contenu qui ne tiendrait pas sur la totalité de l’écran. On parle de scroll bar (barre de défilement en français) pour désigner une barre qui s’affiche sur un coté de l’écran nous permettant d’en faire défiler le contenu.</p>
+
+    <h4>Adresse internet, URL :</h4> 
+    <p>Adresse, le plus souvent affichée en haut du navigateur, qui permet de joindre et de se connecter à un site internet. Chaque site internet à son adresse.</p>
+
+    <h4>Moteur de recherche :</h4> 
+    <p>Service qui permet de faire des recherches sur internet, que ce soit des informations, des images, des vidéos… Google en est le représentant le plus connu.</p>
+
+    <h4>Cookies :</h4> 
+    <p>Petits fichiers qui s’enregistrent durant votre navigation sur le internet, qui permettent en autre aux sites internet de proposer du contenu spécialisé en rapport avec vos précédentes navigations.</p>
+
+    <h4>Bug, Bogue :</h4> 
+    <p>Erreur de conception d’un programme qui affecte son fonctionnement.</p>
+
+    <h4>Chat, tchat :</h4>
+    <p>Messagerie instantanée, qui permet de communiquer en temps réel avec d’autres personnes.</p>
+
+    <h4>Login :</h4>
+    <p>Un nom d’utilisateur qui nous permet de nous connecter à un service. Le plus souvent associé à un mot de passe.</p>
+
+    <h4>E-mails, courriel, courrier électronique :</h4>
+    <p>Message que l’on envoie à travers le réseau à une autre personne, suivant le même principe que le courrier, mais sans les retards de la poste. Celui-ci est reçu dans ce que l’on appelle une « boite mail », une boite aux lettres électronique.</p>
+
+    <h4>Spam :</h4> 
+    <p>Tout message indésirable que l’on est amené à recevoir via internet, l’équivalent des publicités indésirables dans nos boite aux lettres.</p>
+
+    <h2 class='titre2'>Pour aller plus loin</h2>
+    <p>Vous ne connaissez pas certains mots? Vous voulez en apprendre plus sur un sujet? voici deux sites internet qui vous permettront de trouver ce que vous voulez.</p>
+
+    <div class="titre_light"> Wikipédia : </div>
+    <p>Une encyclopédie libre en ligne.</p>
+    <div class="titre_light">Le dictionnaire Larousse</div>
+    <p>Cherchez vos mots comme dans un dictionnaire papier!</p>
+
+    <div class="contenaire">
+        <img src="../assets/images/avatar-ariane-gael 1.svg" alt="">
+        <div class =contenaire_nom>
+            <p>Gael <span id="orange">Sudour-Jeannin</span></p>
+        </div>
+    </div>
+    <p>Stagiaire ACS Dijon</p>
+</div>
+
+
+
+
+
+
+
+
+
+<?php include '../includes/footer.php'; ?>
