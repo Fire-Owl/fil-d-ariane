14ea6256454f371a41855d0021184ed2b8fc0771
JbDruet <jb.druet@codeur.online>
2021-06-02 16:40:27 +0200

    web-et-senior-RUSH

---
+++ pages/articles/web-et-senior/web-et-senior.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../assets/styles/web-et-senior.css">
+    <title>Web-et-senior</title>
+</head>
+<body>
+<div class="mainWrap">
+
+<div></div>
+<div class="firstbloc">
+    <h1>Le web et les seniors</h1>
+    <p>De nos jours, l’utilisation d’internet c’est généralisé et multiplier au quotidien. Pour les personnes qui ont grandi avec ou ceux qui en on vu l’émergence, son utilisation est presque naturelle. Nous pouvons regardez les informations du bout du monde, commander nos courses, communiquer avec nos proches tout ça sans même que nous ayons besoin de réfléchir à comment le faire, c’est devenu instinctif chez la majorité d’entre nous. </p>
+</div>
+<div></div>
+<div><img id="firstblocimg" src="../../../assets/images/web-et-senior_img1.svg" alt=""></div>
+<div></div>
+</div>
+
+
+<div class="mainWrap2">
+
+<div></div>
+<div class="secbloc">
+<img id="secblocimg" src="../../../assets/images/web-et-senior_img2.svg" alt=""> 
+</div>
+<div></div>
+<div>
+    <h1 class="secbloc_h1">Qu’en est-il de ceux pour qui consulter des emails est un fardeau ?</h1>
+    <p class="secbloc_p">Les personnes n’ayant jamais connus Internet sont certains moins nombreuse mais une partie de la population est plus touchée par ce problème, Les personnes dites « âgées ou séniors » représente la part la plus importante de la population française ayant des difficultés avec le numérique et subissent les effets de l’illectronisme. En cette période de crise sanitaires ces problèmes d’accessibilité ont été mis en exergues, l’illectronisme accentue l’isolement des personnes « âgées », selon l’INSEE l’accès au réseau sociaux et aux moyens de communication via Internet permet de garder contact avec sa famille et d’éviter l’isolement.</p>
+
+</div>
+<div></div>
+</div>
+
+<div class="mainWrap3">
+
+<div></div>
+<div class="thirdbloc">
+    <p>De nos jours, l’utilisation d’internet c’est généralisé et multiplier au quotidien. Pour les personnes qui ont grandi avec ou ceux qui en on vu l’émergence, son utilisation est presque naturelle. Nous pouvons regardez les informations du bout du monde, commander nos courses, communiquer avec nos proches tout ça sans même que nous ayons besoin de réfléchir à comment le faire, c’est devenu instinctif chez la majorité d’entre nous. </p>
+</div>
+<div></div>
+<div><img id="thirdblocimg" src="../../../assets/images/web-et-senior_img3.svg" alt=""></div>
+<div></div>
+</div>
+
+<div class="mainWrap4">
+    <h1 id="mainWrap4_title">Quels sont leurs différents usages d’internet ?</h1>
+    <div class="fourthbloc">
+
+    <div></div>
+    <div><img id="fourthblocimg" src="../../../assets/images/web-et-senior_img4.svg" alt="">
+</div>
+    <div></div>
+    <div class="fourthbloc_p"><p>Il y a encore peu, l’usage d’Internet chez les seniors était peu répandu, mais les nouvelles technologies s’ouvrent désormais à toutes les générations. Malgré la persistance de fractures numériques, l’usage de smartphone et d’Internet progresse fortement chez les seniors.<br>
+    
+    </p></div>
+    <div></div>
+    </div>
+</div>
+
+
+<div class="mainWrap5">
+
+        <div></div>
+        <div class="bloc5_p"><p>La majorité des seniors se servent des outils numériques pour des tâches basiques, telles que consulter des mails, la météo, garder contact avec leurs familles via les réseaux sociaux. Cette dernière utilisation est la plus répandue. L’utilisation du numérique est l’un des vecteurs les plus important pour lutter contre l’isolement.<br><br> 
+        Les réseaux sociaux ainsi que les messageries instantanées sont des moyens efficaces pour lutter contre la solitude, particulièrement s’il est empreint d’une dimension affective.</p></div>
+        <div></div>
+        <div><img id="bloc5img" src="../../../assets/images/web-et-senior_img5.svg" alt=""></div>
+        <div></div>
+</div>
+
+
+<div class="mainWrap6">
+
+    <div></div>
+    <div>
+        <img id="bloc6_img" src="../../../assets/images/web-et-senior_img6.svg" alt="">
+    </div>
+    <div></div>
+    <div class="bloc6_p"><p>De plus, Internet peut être une source de plaisir et de divertissement, il leur permet d’avoir un accès plus large à l’information et de manière non négligeable, Internet permet aussi de lutter contre l’ennui. A cela s’ajoute aussi, le fait de pouvoir gagner du temps dans certaines démarches administrative.<br><br>
+    L’utilisation du numérique est un avantage indéniable, mais tout ça dépend de leur niveau de maitrise ainsi que leur confiance dans leur pratique du web. Pour pouvoir inciter plus de séniors à utiliser Internet, il est essentiel de s’intéresser et de comprendre pourquoi certains rencontrent des problèmes et quels sont ces freins.
+</p></div>
+    <div></div>
+
+
+</div>
+
+
+
+</body>
+</html>
