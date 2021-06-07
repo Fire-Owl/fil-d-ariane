2cc841cab86fb8886705ada5c6d8050788d0d741
acs <k.el-yacoubi@codeur.online>
2021-06-07 14:51:56 +0200

    landing page 14h51

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -2,54 +2,66 @@
  
 
 <!-- CAT INFORMATIONS -->
-
+<div class="main__items">
 <div class="container">
-    <div class="container-informations">
-        <p class="bonjour">Bonjour chers voyageurs du web, bienvenue au bout du <span class="fil">« Fil d’Ariane ».</span></p>
+        <div class="container-informations">
+            <p class="bonjour">Bonjour chers voyageurs du web, bienvenue au bout du <span class="fil">« Fil d’Ariane ».</span></p>
 
-        <p class="stagiaires"> Nous sommes une équipe de 4, stagiaires à l’Access Code School de Dijon, notre projet à pour but de sensibiliser les développeurs, les intégrateurs web et le public à <span class="access">l’accessibilité numérique.</span></p> 
-        <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
-    </div> <!-- ferme container informations -->
-    
-    <div class="container-images-informations">
-        <img src="../assets/images/Search-engines-rafiki.svg" alt="search engines rafiki.svg" id="engines">
-    </div> <!-- ferme container__images-informations-->
-</div> <!-- ferme container -->
-
-<!-- CAT PRECONISATIONS-->
-
-<div class="container2">
-    <div class="image-preconisations">
-            <img src="../assets/images/Online world-rafiki.svg" alt="Online world-rafiki.svg" id="world">
-    </div><!--ferme div image-->
-
-    <div class="texte-preconisations">
-            <p class="Outils">Nous avons constaté que malgré une utilisation quotidienne de notre part des outils numérique et d’Internet, certains éprouvaient parfois du mal avec ces outils et une question nous est naturellement venu à l’esprit:</p><br>
-            <p class="Internet">Qu'en est-il des personnes n’ayant pas grandi avec Internet et l’essor du numérique ? </p>
-            <a href="../pages/"><button class="bouton" type="button"> Préconisations</button></a>
-
-    </div>
-</div><!-- ferme container 2-->
-
-<!-- CAT LEXIQUE-->
-
-<div class="container3">
-    <div class="container-lexique">
-        <p class="texte-lexique"> Nous nous sommes penchés sur cette problématique et<br> choisi de nous orienter vers le sujet de <span class="illectronisme">l’illectronisme.<br></span>
+            <p class="stagiaires"> Nous sommes une équipe de 4, stagiaires à l’Access Code School de Dijon, notre projet à pour but de sensibiliser les développeurs, les intégrateurs web et le public à <span class="access">l’accessibilité numérique.</span></p> 
+            <a href="../pages/"><button class="bouton" type="button"> Informations</button></a>
+        </div> <!-- ferme container informations -->
         
-         En cette période de trouble sanitaire et de confinement,<br> nous avons tous eu dans notre entourage, une personne<br> âgée qui s’est retrouvé isolé car elle ne pouvait pas utiliser<br> de nouveaux moyens de communications.<br> 
-         De ce constat amer, nous avons décidé entre autres de parler<br> de sensibiliser au sujet de l’illectronisme, en se concentrant<br> sur l’accessibilité des séniors.<br>
-        Vous trouverez sur ce site divers informations de sensibilisations<br> et de préconisations sur le sujet de l’illectronisme. </p>
-        <a href="../pages/"><button class="bouton" type="button"> Lexique</button></a>
-    </div> <!-- ferme container informations -->
-
-
-
-    <div class="container-images-lexique">
-                    <img src="../assets/images/Mention-rafiki.svg" alt="Mention-rafiki.svg" id="mention">
-    </div> <!-- ferme container__images-informations-->
-
+        
+            <img src="../assets/images/Search-engines-rafiki.svg" alt="search engines rafiki.svg" id="engines">
+        
 </div> <!-- ferme container -->
+
+    <!-- CAT PRECONISATIONS-->
+
+    <div class="container2">
+        
+                <img src="../assets/images/Online world-rafiki.svg" alt="Online world-rafiki.svg" id="world">
+    
+        <div class="texte-preconisations">
+                <p class="Outils">Nous avons constaté que malgré une utilisation quotidienne de notre part des outils numérique et d’Internet, certains éprouvaient parfois du mal avec ces outils et une question nous est naturellement venu à l’esprit:</p><br>
+
+                <div class="texte-preconisations__bouton">
+                    <p class="Internet">Qu'en est-il des personnes n’ayant pas grandi avec Internet et l’essor du numérique ? </p>
+                    <a href="../pages/"><button class="bouton" type="button"> Préconisations</button></a>
+                </div>
+
+        </div>
+    </div><!-- ferme container 2-->
+
+    <!-- CAT LEXIQUE-->
+
+    <div class="container3">
+        <div class="container-lexique">
+            <p class="texte-lexique_bouton"> Nous nous sommes penchés sur cette problématique et<br> choisi de nous orienter vers le sujet de <span class="illectronisme">l’illectronisme.<br></span>
+            
+            En cette période de trouble sanitaire et de confinement,<br> nous avons tous eu dans notre entourage, une personne<br> âgée qui s’est retrouvé isolé car elle ne pouvait pas utiliser<br> de nouveaux moyens de communications.<br> 
+            De ce constat amer, nous avons décidé entre autres de parler<br> de 
+            sensibiliser au sujet de l’illectronisme, en se concentrant<br> sur l’accessibilité des séniors.<br>
+            Vous trouverez sur ce site divers informations de sensibilisations<br> et de préconisations sur le sujet de l’illectronisme. </p>
+            <a href="../pages/"><button class="bouton" type="button"> Lexique</button></a>
+        </div> <!-- ferme container informations -->
+
+
+
+        
+            <img src="../assets/images/Mention-rafiki.svg" alt="Mention-rafiki.svg" id="mention">
+        
+
+    </div> <!-- ferme container -->
+
+</div><!-- ferme div main items-->
+
+<div class="container-fleche">
+<a href="#" class="fleche"><button class="fleche"><img src="../assets/images/btn-haut.svg " alt="fleche"></a>
+            <p class="retour-haut-de-page">Retour haut de page</p>
+
+</div>
+            
 
 
 <?php include '../includes/footer.php'; ?>
