9d26a89c0b94d29ecb0539c928016309cc5745e8
etiennezastko <etiennezastko@hotmail.fr>
2021-06-08 15:47:06 +0200

    commit 08/06/2021

--- pages/à-propos.php
+++ pages/à-propos.php
@@ -29,31 +29,194 @@
                     Dijon.
                 </p>
             </div> <!-- ferme container projet -->
-
-            <img src="../assets/images/à-propos.svg" alt="à-propos.svg" id="img_a_propos">
+            <div class="container_image">
+                <img src="../assets/images/à-propos.svg" alt="à-propos.svg" id="img_a_propos">
+            </div> <!-- ferme container image -->
         </div> <!-- ferme container__projet_et_image -->
-    </div> <!-- ferme container -->
 
-    <div>
+
+    </div> <!-- ferme container -->
+    <div class="parent_element__titre3">
         <h1 class="element__titre3">L'équipe
         </h1>
     </div>
-
     <div>
+        <div class="container_image_2">
+
+            <div></div>
+            <div class="imgwill"><img src="../assets/images/williams.png" alt="williams.png" id="williams.png">
+            </div>
+            <div></div>
+            <div class="imgchloe"> <img src="../assets/images/chloé.png" alt="chloé.png" id="chloé.png"></div>
+            <div></div>
+            <div></div>
+        </div> <!-- ferme container image 2 -->
+        <!-- ouvre container liste 1-->
+        <div class="container_liste">
+            <div></div>
+            <div>
+                <h1 class="titre-liste">Williams Chazal</h1>
+                <div class="texte_liste"><a href="https://github.com/WilliamsChazal">GitHub</a></div>
+                <div>
+                    Missions 1
+                </div>
+                <div>
+                    Missions 2
+                </div>
+                <div>
+                    Missions 3
+                </div>
+            </div>
+
+            <div></div>
+            <div>
+                <h1 class="titre-liste">Chloé Vauthier</h1>
+                <div class="texte_liste"><a href="https://github.com/chloevth/">GitHub</a></div>
+                <div>
+                    Missions 1
+                </div>
+                <div>
+                    Missions 2
+                </div>
+                <div>
+                    Missions 3
+                </div>
+            </div>
+            <div></div>
+            <!-- ferme container liste 1 -->
+        </div>
+        <!-- ouvre container image 3 -->
+        <div class="container_image_3">
+            <div></div>
+            <div class="imgwill"><img src="../assets/images/paule.png" alt="paule" id="paule">
+            </div>
+            <div></div>
+            <div class="imgchloe"> <img src="../assets/images/gael.png" alt="gael" id="gael"></div>
+            <div></div>
+        </div>
+    </div>
+
+    </div>
+    <!-- ferme container image 3 -->
+    <!-- ouvre container liste 2 -->
+    <div class="container_liste">
+        <div></div>
+        <div>
+            <h1 class="titre-liste">Paule Ombiono</h1>
+            <div class="texte_liste"><a href="https://github.com/paule29">GitHub</a></div>
+            <div>
+                Missions 1
+            </div>
+            <div>
+                Missions 2
+            </div>
+            <div>
+                Missions 3
+            </div>
+        </div>
+        <div></div>
+        <div>
+            <h1 class="titre-liste">Gaël SUDOUR-JEANNIN</h1>
+            <div class="texte_liste"><a href="https://github.com/Sastaras">GitHub</a></div>
+            <div>
+                Missions 1
+            </div>
+            <div>
+                Missions 2
+            </div>
+            <div>
+                Missions 3
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+
+    </div>
+    <!-- femre container liste 2 -->
+    <!-- ouvre haut de page -->
+    <div class="container_liste">
+        <div></div>
+        <div>
+            <div></div>
+        </div>
+        <div></div>
+        <div>
+
+            <div></div>
+
+            <div></div>
+            <div></div>
+            <div>
+
+                <a href="#" class=""><button class="retour_haut_de_page">retour haut de page</button>
+                </a>
+
+
+
+
+                </button>
+            </div>
+        </div>
+        <!-- ferme haut de page -->
+    </div>
+
+    <div class="equipe_mobile">
+        <img src="../assets/images/williams.png" alt="williams.png" id="williams">
+        <div class="will-text">
+            <h1 class="titre-liste">Williams Chazal</h1>
+            <p class="texte_liste"><a href="https://github.com/WilliamsChazal">GitHub</a></p>
+            <p>Missions 1</p>
+            <p>Missions 2</p>
+            <p>Missions 3</p>
+        </div>
+        <img src="../assets/images/chloé.png" alt="chloé.png" id="chloé">
+        <div class="chloe-text">
+            <h1 class="titre-liste">Chloé Vauthier</h1>
+            <p class="texte_liste"><a href="https://github.com/chloevth/" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+            <p>Missions 1</p>
+            <p>Missions 2</p>
+            <p>Missions 3</p>
+        </div>
+        <img src="../assets/images/paule.png" alt="paule" id="paule">
+        <div class="paule-text">
+            <h1 class="titre-liste">Paule Ombiono</h1>
+            <p class="texte_liste"><a href="https://github.com/paule29">GitHub</a></p>
+            <p>Missions 1</p>
+            <p>Missions 2</p>
+            <p>Missions 3</p>
+        </div>
+        <img src="../assets/images/gael.png" alt="gael" id="gael">
+        <div class="gael-text">
+            <h1 class="titre-liste">Gael SUDOUR-JEANNIN</h1>
+            <p class="texte_liste"><a href="https://github.com/Sastaras">GitHub</a></p>
+            <p>Missions 1</p>
+            <p>Missions 2</p>
+            <p>Missions 3</p>
+        </div>
+
+
+        <div class="">
+
+
+
+            </button>
+            <img src="../assets/images/brainstorming-rafiki.svg" alt="last_img" id="last_img">
+
+            <a href="#" class=""><button class="fleche"><img src="../assets/images/btn-haut.svg " alt="fleche"></a>
+
+            <p class="retour-haut-de-page">retour haut de page</p>
+
+
+
+        </div>
+
+
 
 
 
 
     </div>
-
-
-
-
-
-
-
-
-
+    <?php include '../includes/footer.php'; ?>
 </body>
 
 </html>
