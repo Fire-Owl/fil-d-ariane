f93db080f74204f72128872dc076345b2772dbab
JbDruet <jb.druet@codeur.online>
2021-06-14 14:36:10 +0200

    animFeature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,10 +9,33 @@ function mobileMenu() {
 
 /* script pour le fil */
 
-window.onscroll = function() { scrollIndicator() } ;
-function scrollIndicator() {
+
+if (document.location.href == 'http://localhost/fil-d-ariane/pages/landing-page.php') {
+  
+  document.getElementById('img').classList.add('img-landing');
+
+  window.onscroll = function() { scrollIndicator() } ;
+
+  function scrollIndicator() {
+          var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+          var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+          var scrolled = (winScroll / height) * 100;
+          document.getElementById("img").style.strokeDashoffset = (1990)-scrolled + "rem";
+        }
+
+} else if (document.location.href == 'http://localhost/fil-d-ariane/pages/lexique.php') {
+            document.getElementById('img').style.display = 'none';
+
+} else {
+  window.onscroll = function() { scrollIndicator() } ;
+  
+  function scrollIndicator() {
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
         var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         var scrolled = (winScroll / height) * 100;
         document.getElementById("img").style.strokeDashoffset = (1990)-scrolled + "rem";
       };
+};
+
+
+/* landing=1656px */
